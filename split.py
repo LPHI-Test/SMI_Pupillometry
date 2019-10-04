ae1a4d94c32aa84e4b45d99ac9aa456c8447f208
ValentinSiderskiy <valentin@realmaxinc.com>
2019-10-04 14:53:25 -0400

    Reading mp4 file
    Wrote python script that can read the mp4 file

---
+++ split.py
@@ -0,0 +1,60 @@
+#Valentin Siderskiy 10/4/2019
+#Integrative Human Physiology Lab
+#Rutgers University
+
+#split.py
+#This function takes the video input recorded by the
+# Night Owel Security stystem, splits the left and right
+# and assigns a timestamp based on the night owel clock.
+
+import cv2
+import numpy as np
+
+def main():
+    # Create a VideoCapture object and read from input file
+    # If the input is the camera, pass 0 instead of the video file name
+    cap = cv2.VideoCapture('SMI_Pupilometry_Test.mp4')
+
+    w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    fcount = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+
+    print("WIDTH: ", w)
+    print("HEIGHT: ", h)
+    print("FPS: ", fps)
+    print("FCOUNT: ", fcount)
+
+
+
+    # Check if camera opened successfully
+    if (cap.isOpened()== False):
+      print("Error opening video stream or file")
+
+    #   Read until video is completed
+    while(cap.isOpened()):
+      # Capture frame-by-frame
+      ret, frame = cap.read()
+
+      if ret == True:
+
+
+          # Display the resulting frame
+          cv2.imshow('Frame',frame)
+
+          # Press Q on keyboard to  exit
+          if cv2.waitKey(25) & 0xFF == ord('q'):
+              break
+
+          # Break the loop
+      else:
+        break
+
+    # When everything done, release the video capture object
+    cap.release()
+
+    # Closes all the frames
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+        main()
