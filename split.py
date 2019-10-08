9d3df078586dbd0863e324941541ea4b32ca1a39
ValentinSiderskiy <valentin@realmaxinc.com>
2019-10-08 08:21:17 -0400

    Resized Display + Changed Video
    Resized Display:  Resized the display to fit monitor resolution.
    Changed Test Video

--- split.py
+++ split.py
@@ -9,6 +9,30 @@
 
 import cv2
 import numpy as np
+import time
+
+def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+
+    if width is None and height is None:
+        return image
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    return cv2.resize(image, dim, interpolation=inter)
+
+
+#Operation a freame form Night Owel security stystem use OCR to find out time of
+#recording
+#Input: frame
+#Output: time
+def getTime(frame,timestamp):
+    print("time")
 
 def main():
     # Create a VideoCapture object and read from input file
@@ -19,13 +43,12 @@ def main():
     h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
     fps = cap.get(cv2.CAP_PROP_FPS)
     fcount = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+    timestamp = time.clock()
 
     print("WIDTH: ", w)
     print("HEIGHT: ", h)
     print("FPS: ", fps)
     print("FCOUNT: ", fcount)
-
-
 
     # Check if camera opened successfully
     if (cap.isOpened()== False):
@@ -38,9 +61,13 @@ def main():
 
       if ret == True:
 
+          getTime(frame,timestamp)
+
+          resized_frame = ResizeWithAspectRatio(frame, width = 640) #Resize by
+          # resized_frame = ResizeWithAspectRatio(frame, height = 480)
 
           # Display the resulting frame
-          cv2.imshow('Frame',frame)
+          cv2.imshow('Frame',resized_frame)
 
           # Press Q on keyboard to  exit
           if cv2.waitKey(25) & 0xFF == ord('q'):
