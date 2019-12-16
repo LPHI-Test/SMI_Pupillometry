72674a893a707429a5ba62967334efde8d3f2e3b
ValentinSiderskiy <valentin@realmaxinc.com>
2019-12-16 16:49:44 -0500

    pylint changes

--- split.py
+++ split.py
@@ -19,7 +19,9 @@ FONT = cv2.FONT_HERSHEY_SIMPLEX
 SMI_DIM = (720, 480) # Dimension from SMI System
 DEFAULT_FILE_NAME = 'V1219_20850228_234726_Dilation2.mp4'
 
-def plotRadialPerimeter(leftRads,leftRadius,rightRads,rightRadius):
+
+def plot_radial_perimeter(left_rads, left_radius, right_rads, right_radius):
+    """plots radians over radius for the left and right eye"""
     plt.subplot(121)
     plt.clf()
     plt.subplot(122)
@@ -28,78 +30,80 @@ def plotRadialPerimeter(leftRads,leftRadius,rightRads,rightRadius):
     plt.subplot(121)
     plt.ion()
     plt.axis([-4, 4, 35, 60])
-    plt.plot(leftRads, leftRadius, 'ro')
+    plt.plot(left_rads, left_radius, 'ro')
     plt.ylabel('left radius (pixels)')
     plt.xlabel('angle (radians)')
     plt.subplot(122)
     plt.ion()
     plt.axis([-4, 4, 35, 60])
-    plt.plot(rightRads, rightRadius, 'ro')
+    plt.plot(right_rads, right_radius, 'ro')
     plt.ylabel('right radius (pixels)')
     plt.xlabel('angle (radians)')
     plt.show()
     plt.pause(0.05)
-def resizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
+
+def resize_with_aspect_ratio(image, width=None, height=None, inter=cv2.INTER_AREA):
+    """Resizes image to particular width or height but keeps aspect ratio"""
     dim = None
-    (h, w) = image.shape[:2]
+    (image_h, image_w) = image.shape[:2]
 
     if width is None and height is None:
         return image
     if width is None:
-        r = height / float(h)
-        dim = (int(w * r), height)
+        ratio = height / float(image_h)
+        dim = (int(image_w * ratio), height)
     else:
-        r = width / float(w)
-        dim = (width, int(h * r))
+        ratio = width / float(image_w)
+        dim = (width, int(image_h * ratio))
 
     return cv2.resize(image, dim, interpolation=inter)
 
-def displayLeft(left):
-    # Display the Left Eye
-    resized_left = resizeWithAspectRatio(left, width =640) #Resize by
-    lString = "Left"
-    cv2.putText(resized_left, lString, (0, 20), FONT, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
-    cv2.imshow('Left Eye',resized_left)
+def display_left(left):
+    """ Display the Left Eye"""
+    resized_left = resize_with_aspect_ratio(left, width=640) #Resize by\
+    # pylint: disable=C0103
+    L_STRING = "Left"
+    cv2.putText(resized_left, L_STRING, (0, 20), FONT, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+    cv2.imshow('Left Eye', resized_left)
     # resized_frame = ResizeWithAspectRatio(frame, height = 480)
 
 
-def displayRight(right):
-    # Display the Right Eye
-    resized_right = resizeWithAspectRatio(right, width = 640) #Resize by
-    rString = "Right"
-    cv2.putText(resized_right, rString, (0, 20), FONT, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
-    cv2.imshow('Right Eye',resized_right)
+def display_right(right):
+    """Display the Right Eye"""
+    resized_right = resize_with_aspect_ratio(right, width=640) #Resize by
+    # pylint: disable=C0103
+    R_STRING = "Right"
+    cv2.putText(resized_right, R_STRING, (0, 20), FONT, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+    cv2.imshow('Right Eye', resized_right)
     # resized_frame = ResizeWithAspectRatio(frame, height = 480)
 
-def displayMain(frame,cap):
-    #Display Main
+def display_main(frame, cap):
+    """Display Main#"""
     fcount = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-    resized_frame = resizeWithAspectRatio(frame, width = 640) #Resize by
+    resized_frame = resize_with_aspect_ratio(frame, width=640) #Resize by
     # resized_frame = ResizeWithAspectRatio(frame, height = 480)
 
-    framePosition = cap.get(cv2.CAP_PROP_POS_FRAMES)
-    fpString = "Frame " + str(framePosition) + '/' + str(fcount)
-    cv2.putText(resized_frame, fpString, (230, 350), FONT, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
-
-    quitString = "Press Q to Quit"
-    cv2.putText(resized_frame, quitString, (230, 250), FONT, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+    frame_position = cap.get(cv2.CAP_PROP_POS_FRAMES)
+    fp_string = "Frame " + str(frame_position) + '/' + str(fcount)
+    cv2.putText(resized_frame, fp_string, (230, 350), FONT, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+    # pylint: disable=C0103
+    QUIT_STRING = "Press Q to Quit"
+    cv2.putText(resized_frame, QUIT_STRING, (230, 250), FONT, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
 
     # Display the resulting frame
-    cv2.imshow('Frame',resized_frame)
+    cv2.imshow('Frame', resized_frame)
 
-
-#Operation a freame form Night Owel security stystem use OCR to find out time of
-#recording
-#Input: frame
-#Output: time
-def getTime(frame,timestamp):
+# pylint: disable=W0613
+def get_time(frame, timestamp):
+    """Get time from Night Owel security syste use OCT
+    Input: frame
+    Output: time"""
     print("time")
 
-#input frame: -1, the whole file, else a particular frame
-def main(frame = -1, filename = DEFAULT_FILE_NAME):
-
-    if(int(framenum) == -1):
-        debug = 0;
+def main(frame=-1, filename=DEFAULT_FILE_NAME):
+    """#input frame: -1, the whole file, else a particular frame"""
+    if int(framenum) == -1:
+        debug = 0
     else:
         debug = 3
 
@@ -107,80 +111,79 @@ def main(frame = -1, filename = DEFAULT_FILE_NAME):
     # If the input is the camera, pass 0 instead of the video file name
     cap = cv2.VideoCapture(filename)
 
-    w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    frame_w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = cap.get(cv2.CAP_PROP_FPS)
     fcount = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-    timestamp = time.clock()
 
-    is_before_first = True;
+    is_before_first = True
 
-    print("WIDTH: ", w)
-    print("HEIGHT: ", h)
+    print("WIDTH: ", frame_w)
+    print("HEIGHT: ", frame_h)
     print("FPS: ", fps)
     print("FCOUNT: ", fcount)
 
     # Check if camera opened successfully
-    if (cap.isOpened()== False):
-      print("Error opening video stream or file")
+    if not cap.isOpened():
+        print("Error opening video stream or file")
 
     #   Read until video is completed
     while(cap.isOpened()):
       # Capture frame-by-frame
 
-      if(int(framenum) != -1):
-          cap.set(cv2.CAP_PROP_POS_FRAMES, int(framenum)-1)
+        if(int(framenum) != -1):
+            cap.set(cv2.CAP_PROP_POS_FRAMES, int(framenum)-1)
 
-      ret, frame = cap.read()
+        ret, frame = cap.read()
 
-      if ret == True:
+        if ret == True:
 
-          print(cap.get(cv2.CAP_PROP_POS_FRAMES))
-          if framenum == -1 or int(framenum) == int(cap.get(cv2.CAP_PROP_POS_FRAMES)) :
-              #getTime(frame,timestamp)
-              leftImageOwl =  frame[10:int(h/2),1:int(w/2)]
-              rightImageOwl = frame[10:int(h/2),int(w/2+1):w]
+            print(cap.get(cv2.CAP_PROP_POS_FRAMES))
+            if framenum == -1 or int(framenum) == int(cap.get(cv2.CAP_PROP_POS_FRAMES)) :
+                #get_time(frame,timestamp)
+                leftImageOwl =  frame[10:int(frame_h/2),1:int(frame_w/2)]
+                rightImageOwl = frame[10:int(frame_h/2),int(frame_w/2+1):frame_w]
 
-              leftImage = cv2.resize(leftImageOwl, SMI_DIM, interpolation=cv2.INTER_AREA)
-              rightImage = cv2.resize(rightImageOwl, SMI_DIM, interpolation=cv2.INTER_AREA)
+                leftImage = cv2.resize(leftImageOwl, SMI_DIM, interpolation=cv2.INTER_AREA)
+                rightImage = cv2.resize(rightImageOwl, SMI_DIM, interpolation=cv2.INTER_AREA)
 
 
-              if( debug > 0):
-                  print("Left Size: ", leftImage.shape)
-                  print("Right Size: ", leftImage.shape)
+                if( debug > 0):
+                    print("Left Size: ", leftImage.shape)
+                    print("Right Size: ", leftImage.shape)
 
 
               #save nth frame to a file
-              if(framenum == -1):
-                  if(is_before_first and cap.get(cv2.CAP_PROP_POS_FRAMES) == 45):
-                      is_before_first = False
-                      cv2.imwrite("left.bmp",leftImage)
-                      cv2.imwrite("right.bmp",rightImage)
-              else:
-                  if(is_before_first and cap.get(cv2.CAP_PROP_POS_FRAMES) == framenum):
-                      is_before_first = False
-                      cv2.imwrite("left.bmp",leftImage)
-                      cv2.imwrite("right.bmp",rightImage)
+                if(framenum == -1):
+                    if(is_before_first and cap.get(cv2.CAP_PROP_POS_FRAMES) == 45):
+                        is_before_first = False
+                        cv2.imwrite("left.bmp",leftImage)
+                        cv2.imwrite("right.bmp",rightImage)
+                else:
+                    if(is_before_first and cap.get(cv2.CAP_PROP_POS_FRAMES) == framenum):
+                        is_before_first = False
+                        cv2.imwrite("left.bmp",leftImage)
+                        cv2.imwrite("right.bmp",rightImage)
 
 
-              leftImageEdit, leftRads, leftRadius = pupillometry(leftImage,debug)
-              rightImageEdit, rightRads, rightRadius = pupillometry(rightImage,debug)
+                leftImageEdit, left_rads, left_radius = pupillometry(leftImage,debug)
+                rightImageEdit, right_rads, right_radius = pupillometry(rightImage,debug)
 
-              displayMain(frame,cap)
-              displayLeft(leftImageEdit)
-              displayRight(rightImageEdit)
-              plotRadialPerimeter(leftRads,leftRadius,rightRads,rightRadius)
+                display_main(frame, cap)
+                display_left(leftImageEdit)
+                display_right(rightImageEdit)
+                plot_radial_perimeter(left_rads,left_radius,right_rads,right_radius)
 
-              # Press Q on keyboard to  exit
-              if cv2.waitKey(1) & 0xFF == ord('q'):
-                  break
+                # Press Q on keyboard to  exit
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
 
               # Break the loop
-      else:
-        break
+        else:
+            break
 
-      if cap.get(cv2.CAP_PROP_POS_FRAMES) == int(framenum):
-          break
+        if cap.get(cv2.CAP_PROP_POS_FRAMES) == int(framenum):
+            break
 
     # When everything done, release the video capture object
     cap.release()
