efc9bb18255248ce895ea6bce91f99ee33f9db05
etan18 <erintan4@gmail.com>
2020-08-10 11:08:08 -0400

    Update software_check.py

--- software_check.py
+++ software_check.py
@@ -1,16 +1,20 @@
-import cv2
 import sys
 import subprocess
 import pkg_resources
 
+try:
+    import cv2
+except ImportError:
+    print("Error: cv2 not installed")
+
 # collects user versions
-pyVersion = sys.version[0:5]
-cvVersion = cv2.__version__
+pyVersion = sys.version[0:4]
+cvVersion = cv2.__version__[0:4]
 
 # constants
 # change according to version
-pythonCheck = "3.7.4"
-cvCheck = "4.1.1"
+pythonCheck = "3.7."
+cvCheck = "4.1."
 
 # check python version
 if(pyVersion != pythonCheck):
@@ -23,7 +27,7 @@ if(cvVersion != cvCheck):
     print("Install OpenCV ", cvCheck)
 
 # check for libraries
-required = {'math', 'matplotlib', 'numpy'}
+required = {'matplotlib', 'numpy'}
 installed = {pkg.key for pkg in pkg_resources.working_set}
 missing = required - installed
 
