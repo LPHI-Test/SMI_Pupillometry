cc4193d3580bdefc23b19d9989354c2a928c776f
etan18 <erintan4@gmail.com>
2020-08-15 19:45:41 -0400

    Rearranged code to accomodate all cases, added notLibCheck() to print missing libs
    Todo:
    - check if python is installed
    - minor printing bug on some cases (prints with parentheses and 
    quotations)
    - update to pylint standards

--- software_check.py
+++ software_check.py
@@ -10,8 +10,6 @@ cvCheck = "4.1"
 def Python_Check():
     # check python version
     if(pyVersion != pythonCheck):
-        print("Error: using Python ", pyVersion)
-        print("Install Python ", pythonCheck)
         return False
     return True
 
@@ -19,8 +17,6 @@ def Python_Check():
 def CV_Check():
     # check OpenCV version
     if(cvVersion != cvCheck):
-        print("Error: using OpenCV ", cvVersion)
-        print("Install OpenCV ", cvCheck)
         return False
     return True
 
@@ -32,27 +28,45 @@ def Library_Check():
     missing = required - installed
 
     if missing:
-        print("Error: missing modules ", missing)
         return False
 
     return True
 
+def notLibCheck():
+    required = {'matplotlib', 'numpy'}
+    installed = {pkg.key for pkg in pkg_resources.working_set}
+    missing = required - installed
+    print("Error: missing modules ", missing)
 
 def main():
-    if(Python_Check()):
-        if(CV_Check()):
-            if(Library_Check()):
-                print("Software Check: Passed")
+    if(Python_Check() and CV_Check() and Library_Check()):
+        print("Software Check: Passed")
+    else:
+        if(not Python_Check()):
+            print("Error: using Python ", pyVersion)
+            print("Install Python ", pythonCheck)
+        if(not CV_Check()):
+            print("Error: using OpenCV ", cvVersion)
+            print("Install OpenCV ", cvCheck)
+        if(not Library_Check()):
+            notLibCheck()
 
 
 if __name__ == "__main__":
+
+    # collects user versions
+    pyVersion = sys.version[0:3]
+
     try:
         import cv2
         cvVersion = cv2.__version__[0:3]
     except ImportError:
         print("Error: cv2 not installed")
-
-    # collects user versions
-    pyVersion = sys.version[0:3]
+        if(not Python_Check()):
+            print("Error: using Python ", pyVersion)
+            print("Install Python ", pythonCheck)
+        if(not Library_Check()):
+            notLibCheck()
+        exit()
 
     main()
